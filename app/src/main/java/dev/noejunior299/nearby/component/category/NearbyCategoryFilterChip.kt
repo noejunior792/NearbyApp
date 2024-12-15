908977ffcf505f01523bc02613d8f963719c1cc2
noejunior792 <noedombaxe@gmail.com>
2024-12-15 14:56:21 +0100

    add NearbyCategoryFilterChip button component

--- app/src/main/java/dev/noejunior299/nearby/component/category/NearbyCategoryFilterChip.kt
+++ app/src/main/java/dev/noejunior299/nearby/component/category/NearbyCategoryFilterChip.kt
@@ -1,8 +1,23 @@
 package dev.noejunior299.nearby.component.category
 
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.noejunior299.nearby.data.model.NearbyCategory
+import dev.noejunior299.nearby.ui.theme.Gray300
+import dev.noejunior299.nearby.ui.theme.Gray400
+import dev.noejunior299.nearby.ui.theme.GreenBase
+import dev.noejunior299.nearby.ui.theme.Typography
 
 @Composable
 fun NearbyCategoryFilterChip(
@@ -11,5 +26,56 @@ fun NearbyCategoryFilterChip(
     isSelected: Boolean,
     onClick: (isSelected: Boolean) -> Unit
 ) {
+    FilterChip(
+        modifier = modifier
+            .padding(2.dp)
+            .heightIn(min = 36.dp),
+        elevation = FilterChipDefaults.filterChipElevation(
+            elevation = 8.dp
+        ),
+        leadingIcon =  {
+            category.icon?.let {
+                Icon(
+                   modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = it),
+                    tint =  if (isSelected) Color.White else Gray400,
+                    contentDescription = "Ícone de Filtro da Categoria"
+                )
+            }
+        },
+        border = FilterChipDefaults.filterChipBorder(
+          enabled = false,
+            selected = isSelected,
+            disabledBorderColor = Gray300,
+            borderWidth = 1.dp,
+            selectedBorderWidth = 0.dp,
+            selectedBorderColor = Color.Transparent
+        ),
+        colors = FilterChipDefaults.filterChipColors(
+            containerColor =  Color.White ,
+            selectedContainerColor = GreenBase,
+        ),
+        selected = isSelected,
+        onClick = { onClick(isSelected) },
+        label = {
+            Text(
+                text = category.name,
+                style = Typography.bodyMedium,
+                color = if (isSelected) Color.White else Gray400
+            )
+        }
+    )
+}
 
+@Preview
+@Composable
+fun NearbyCategoryFilterChipPreview() {
+    NearbyCategoryFilterChip(
+   category = NearbyCategory (
+       id = "1",
+       name = "Alimentação"
+   ),
+        isSelected = true,
+        onClick = {}
+    )
 }
--- app/src/main/java/dev/noejunior299/nearby/data/model/NearbyCategoryFilterChipView.kt
+++ app/src/main/java/dev/noejunior299/nearby/data/model/NearbyCategoryFilterChipView.kt
@@ -8,8 +8,13 @@ enum class NearbyCategoryFilterChipView (
     @DrawableRes val icon: Int,
 ){
     ALIMENTACAO(description="Alimentação", icon= R.drawable.ic_tools_kitchen_2),
-    COMPRAS(description="Compras", icon= R.drawable.ic_shopping_cart),
-    HOSPEDAGEM(description = "HOSPEDAGEM", icon= R.drawable.ic_bed);
+    COMPRAS(description="Compras", icon= R.drawable.ic_shopping_bag),
+    HOSPEDAGEM(description = "Hospedagem", icon= R.drawable.ic_bed),
+    SUPERMERCADO(description = "Supermercado", icon= R.drawable.ic_shopping_cart),
+    ENTRETENIMENTO(description = "Entretenimento", icon= R.drawable.ic_movie),
+    FARMACIA(description = "Farmácia", icon= R.drawable.ic_first_aid_kit),
+    COMBUSTIVEL(description = "Combustivel", icon= R.drawable.ic_gas_station),
+    PADARIA(description = "Padaria", icon= R.drawable.ic_bakery);
 
     companion object {
         fun fromDescription(
