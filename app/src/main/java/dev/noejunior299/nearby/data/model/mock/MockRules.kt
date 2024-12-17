17826692649d4f32ba4259e074ba005afc88954d
noejunior792 <noedombaxe@gmail.com>
2024-12-17 22:55:02 +0100

    add MarketDetailsRules

---
+++ app/src/main/java/dev/noejunior299/nearby/data/model/mock/MockRules.kt
@@ -0,0 +1,16 @@
+package dev.noejunior299.nearby.data.model.mock
+
+import dev.noejunior299.nearby.data.model.NearbyRule
+
+val mockRules = listOf(
+    NearbyRule(
+        id = "rule1",
+        description = "Regra 1",
+        marketId = "market1"
+    ),
+    NearbyRule(
+        id = "rule2",
+        description = "Regra 2",
+        marketId = "market1"
+    ),
+)
