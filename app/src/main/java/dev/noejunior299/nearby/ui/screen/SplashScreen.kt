13145f0155ddc5b5b426411cea9b22206e80852d
noejunior792 <noedombaxe@gmail.com>
2024-12-25 12:42:47 +0100

    add splashscreen

---
+++ app/src/main/java/dev/noejunior299/nearby/ui/screen/SplashScreen.kt
@@ -0,0 +1,39 @@
+package dev.noejunior299.nearby.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import dev.noejunior299.nearby.R
+import dev.noejunior299.nearby.theme.GreenLight
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(GreenLight)
+            .fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.img_logo_logo_logo_text),
+            contentDescription = "Imagem do Logo"
+        )
+        Image(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            painter = painterResource(id = R.drawable.bg_splash_screen),
+            contentDescription = "Imagem de Fundo"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
